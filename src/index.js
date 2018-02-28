a48a4f9fdd6f1bdd3d2a4146c4e85bc86959218e
iFdit <lyliyangwc@gmail.com>
2018-02-28 18:10:38 +0800

    move static method defined

--- src/index.js
+++ src/index.js
@@ -8,4 +8,9 @@ Promise.prototype.catch = function (onrejected) {
   return then.call(this, null, onrejected)
 }
 
+// Static Method
+Promise.resolve = (value) => new Promise((resolve) => resolve(value))
+Promise.reject = (value) => new Promise((resolve, reject) => reject(value))
+
+
 module.exports = Promise
